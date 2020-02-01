2e31e77e8a4dc308c101e2b7e8d835ba3176086f
donnydmp <donny.dmp@gmail.com>
2020-02-01 08:46:11 +0700

    penambahan exercise terakhir

---
+++ Exercise 11.js
@@ -0,0 +1,18 @@
+//Logic Challenge - Balik Kata
+
+function balikKata(kata) {
+    var balikKata = kata;
+    var kata1 = '';
+    for (var i = kata.length - 1; i >= 0; i--) {
+     kata1 = kata1 + balikKata[i];
+    }
+    
+    return kata1;
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
---
+++ Exercise 12.js
@@ -0,0 +1,15 @@
+// Logic Challenge - Konversi Menit
+
+function konversiMenit(menit) {
+  if(menit%60 <= 9){
+   return Math.floor(menit/60) + ' : 0' + menit%60;
+ }
+return Math.floor(menit/60) + ' : ' + menit%60;
+}
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
